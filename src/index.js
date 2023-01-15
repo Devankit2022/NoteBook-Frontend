e933c8072d1ac12a2fa7a9ade10087f271df5df7
Devankit2022 <devankit2022@gmail.com>
2023-01-15 20:32:05 +0530

    fix: build error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store } from './application/store';
 import { Provider } from 'react-redux';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
